1e7b532207ddaf12966e0de17e3ce19267427448
MrinalCom <Mrinal.Anand2021@iem.edu.in>
2025-03-28 01:33:06 +0530

    node production

--- jwt/AuthToken.js
+++ jwt/AuthToken.js
@@ -5,11 +5,13 @@ const createTokenAndSaveCookies = async (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
     expiresIn: "30d",
   });
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
-    secure: false,
-    sameSite: "lax",
-    path: "/", // Ensure the cookie is available throughout the site
+    httpOnly: true,
+    secure: isProduction, // Secure only in production
+    sameSite: isProduction ? "none" : "lax", // Required for cross-origin HTTPS
+    path: "/",
   });
   await User.findByIdAndUpdate(userId, { token });
   return token;
